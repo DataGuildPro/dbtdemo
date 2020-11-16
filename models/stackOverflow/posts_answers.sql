a1ec2dc4288a19eecf189a392c3d8c6729036bd3
anat Shwartzman <anat@dataguild.pro>
2020-11-16 12:01:06 +0000

    incremental load

--- models/stackOverflow/posts_answers.sql
+++ models/stackOverflow/posts_answers.sql
@@ -1,9 +1,17 @@
 {{
     config(
-        materialized='incremental'
+        materialized='incremental',
+        unique_key ='id'
     )
 }}
 
+select id , title, answer_count, comment_count, creation_date, favorite_count, last_activity_date, score, tags, view_count 
+from stackoverflow.posts_answers
 
-select * from `bigquery-public-data.stackoverflow.posts_answers`
-where 
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  where last_activity_date >= (select max(last_activity_date) from {{ this }})
+
+{% endif %}
+ 
