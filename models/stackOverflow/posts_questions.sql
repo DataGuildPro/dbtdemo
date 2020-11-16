4ac09bc7cc51d82c77a765f9027d99835f3384fc
anat Shwartzman <anat@dataguild.pro>
2020-11-16 09:27:16 +0000

    approval_rate

--- models/stackOverflow/posts_questions.sql
+++ models/stackOverflow/posts_questions.sql
@@ -2,9 +2,9 @@
 -- {{ config(materialized='table') }}
 
 with questions as (
-select  q.id as question_id,q.title, q.accepted_answer_id, q.answer_count, q.creation_date , split(q.tags,'|') as tag 
+select  q.id as question_id,q.title, q.accepted_answer_id, q.answer_count, q.creation_date , split(q.tags,'|') as tags 
 from `bigquery-public-data.stackoverflow.posts_questions` q 
 )
 
-select * except (tag)
-from questions q join UNNEST (tag) as question_tag
+select * except (tags)
+from questions q join UNNEST (tags) as tag
